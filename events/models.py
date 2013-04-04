fa40ebba5e1d7c8b65f37110068d9f06a1034e89
Prakash Gurung <prakashgrg18@gmail.com>
2013-04-04 12:27:38 +0100

    prakash test commit

--- events/models.py
+++ events/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-
+# hello i am prakash
 class Promotion(models.Model):
   title=models.CharField(max_length=200)
   weekday=models.CharField(max_length=9,null=True)
