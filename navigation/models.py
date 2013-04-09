31083f83d0d12dfcdc2bb6a24e3fc880a758edf2
Prakash Gurung <prakashgrg18@gmail.com>
2013-04-09 17:16:50 +0100

    fixed the image upload n display in front end but the contact page returns error when submitted about CSRF.. need to fix them

--- navigation/models.py
+++ navigation/models.py
@@ -1,3 +1,10 @@
-from django.db import models
+from django import forms
 
-# Create your models here.
+class ContactForm(forms.Form):
+  name = forms.CharField(max_length=100)
+  subject = forms.CharField(required=False)
+  message = forms.CharField(required=True)
+  sender = forms.EmailField(required=True)
+  email_copy = forms.BooleanField(required=False)
+  def __unicode__(self):
+    return self.title
