0d54f72b4a53927d6542be12e5abc371b09aca2c
jake <mail@jakepearse.com>
2013-05-28 13:20:15 +0100

    made mailreport.py create a csv file and email it to address supplied as args

--- events/management/commands/mailreport.py
+++ events/management/commands/mailreport.py
@@ -5,22 +5,19 @@ import datetime
 
 class Command(BaseCommand):
   args ='<emailaddress emailaddress ..>'  
-  help = 'Closes the specified poll for voting'
+  help = 'generate and mail a ticket report'
   def handle(self, *args, **options):
     today = datetime.date.today()
+    time = datetime.datetime.now()
     try:
-      ticketQS = Ticket.objects.filter(event__date=today)
-    except Event.DoesNotExist:
-      raise CommandError('no tickets for %s' % today)
-    headers='"order_ref","first_name",last_name","postcode","eventDate","quantity"'
-    ticket_list =[]
-    mail_string =""
-    for ticket in ticketQS:
-      ticket_list.append('"%s","%s","%s","%s","%s","%s"\n'%(ticket.id,ticket.first_name,ticket.last_name,ticket.postcode,ticket.event.date,ticket.quantity))
-    for i in ticket_list:
-      self.stdout.write(i)
-      mail_string +=i
+      ticketQS = Ticket.objects.filter(event__date=today).filter(status='confirmed')
+    except:
+      raise CommandError('no event for %s' % today)
+    with open('%s.csv'%time,'w') as file:
+      file.write('"order_ref","first_name",last_name","postcode","eventDate","quantity"\n')
+      for ticket in ticketQS:
+        file.write('"%s","%s","%s","%s","%s","%s"\n'%(ticket.id,ticket.first_name,ticket.last_name,ticket.postcode,ticket.event.date,ticket.quantity))
     recipients =[i for i in self.args]
-    mail = EmailMessage("door-list for %s"%today, ".csv attached", "ticket-list", recipients)
-    mail.attach('%s.csv'%today, mail_string, 'text/csv')
+    mail = EmailMessage("door-list for %s"%today, "%s.csv attached"%time, "ticket-list", recipients)
+    mail.attach_file(path="%s.csv"%time,mimetype="text/csv")
     mail.send()
